e6b964b60ddc2a84f086452204c7c819ebc141fa
Brianod94 <brianoro312@gmail.com>
2025-02-26 19:56:36 -0500

    Update Valor_milimetros.py

--- Valor_milimetros.py
+++ Valor_milimetros.py
@@ -1,1 +1,10 @@
-dawwadawd
+valor = int(input('Ingrese el valor en milimetros'))
+
+metros = valor//1000
+centimetros = (valor%1000) //10
+milimetros = valor %10
+
+print('Valor ingresado en milimetros', valor, ' equivale a: ')
+print(' metros: ', metros)
+print(' centimetros: ', centimetros)
+print(' milimetros: ', milimetros)
