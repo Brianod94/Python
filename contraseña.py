0866280692ba3d93120c84cd781fefa8f299a664
Brianod94 <brianoro312@gmail.com>
2025-02-28 10:34:27 -0500

    nuevos

---
+++ contraseña.py
@@ -0,0 +1,7 @@
+contraseña=(input("ingrese su contraseña"))
+contraseña_guardada="1q2w3e4r5t6y"
+if contraseña==contraseña_guardada:
+    print("acceso concedido")
+else:
+    print("error contraseña")
+                       
---
+++ numero_mayor.py
@@ -0,0 +1,7 @@
+numero1=int(input("ingrese un numero"))
+numero2=int(input("ingrese un numero"))
+if numero1>numero2:
+    print("este numero es mayor",numero1)
+elif numero2>numero1:
+    print("este numero es mayor",numero2)
+else:print("ambos son iguales")
