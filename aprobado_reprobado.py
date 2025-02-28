303675711ab8232e5950ab3b5084082b2986221b
Brianod94 <brianoro312@gmail.com>
2025-02-28 09:09:26 -0500

    condicionales

---
+++ aprobado_reprobado.py
@@ -0,0 +1,24 @@
+#Un alumno desea saber cuál será su calificación final en la materia de Algoritmos y si aprobó o no.
+#  Dicha calificación se compone de los siguientes porcentajes:
+	#55% del promedio de sus tres calificaciones parciales.
+	#30% de la calificación del examen final.
+	#15% de la calificación de un trabajo final
+notas_paciales1=float(input("ingrese 1ra nota: = "))
+notas_paciales2=float(input("ingrese 2da nota: = "))
+notas_paciales3=float(input("ingrese 3ra nota: = "))   
+examen_final=float(input("nota de examen final: = "))  #nota del examen final
+trabajo_final=float(input("nota trabajo final: = "))   #nota del trabajo final
+promedio_parcial=(notas_paciales1+notas_paciales2+notas_paciales3)/3 #sumatoria de la notas parciales
+print("promedioparcial",promedio_parcial) 
+promedio_final=(promedio_parcial*0.55)	#
+print("promedio_final:",promedio_final)
+nota_exa_final=(examen_final*0.3)		#calificaciones con su respectivo porcentaje
+print("examenfinal",nota_exa_final)
+notas_trab_fin=(trabajo_final*0.15)		#
+print("trabajofinal",notas_trab_fin)
+calificacion_final=promedio_final+nota_exa_final+notas_trab_fin	#sumatoria de los porcentajes
+print("tu calificacion final es: = ",calificacion_final)
+if calificacion_final>3:
+    print("has aprobado la materia")
+else: 
+    print("has reprobado la materia")
--- nota_final_semestre.py
+++ nota_final_semestre.py
@@ -1,8 +1,11 @@
 notacorte1=float(input('ingrese nota 1er corte:'))
 notacorte2=float(input('ingrese nota 2do corte:'))
 notacorte3=float(input('ingrese nota 3er corte:'))
-promedio1er=notacorte1*0.3
+promedio1er=notacorte1*0.55
+print(promedio1er)
 promedio2do=notacorte2*0.3
-promedio3er=notacorte3*0.4
-promedio_final=(notacorte1+notacorte2+notacorte3)/3
+print(promedio2do)
+promedio3er=notacorte3*0.15
+print(promedio3er)
+promedio_final=(promedio1er+promedio2do+promedio3er)
 print('el promedio final del estudiante es:',promedio_final)
