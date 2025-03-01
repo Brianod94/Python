a67008eac97683e2379b0881fdae4d54821d9cff
Brian Orozco <brianoro312@gmail.com>
2025-03-01 11:41:06 -0500

    marzo 1

---
+++ divisores_dividendo.py
@@ -0,0 +1,6 @@
+dividendo = int(input("Digite o dividendo: "))
+divisor = int(input("Digite o divisor: "))
+if divisor == 0:
+    print("!error! no se puede dividir por 0. ")
+resultado = (dividendo/divisor)
+print(resultado)
