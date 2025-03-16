d9558333ff77e88dc3491fa9fb1748e32e4b1004
Bianod94 <brianoro312@gmail.com>
2025-03-16 12:35:40 -0500

    practicas

--- practicas3/sistema_vacional.py
+++ practicas3/sistema_vacional.py
@@ -1,10 +1,14 @@
 print("===========sistema de vacaciones ===============")
 clave_departamento=int(input("ingrese el departamento al cual pertenece\n1. Atencion al cliente\n2. Logistica\n3. Gerencia\nOpcion: "))
- 
+if clave_departamento==4:
+     print("¡ESTE DEPARTAMENTO NO ESTA DISPONIBLE!")
+     exit()
+trabajador=input("nombre del trabajdor. ")
+años=float(input("digite años de antiguedad. "))
 #ATENCION AL CLIENTE
 if clave_departamento==1:
     trabajador=input("nombre del trabajdor. ")
-    años=int(input("digite años de antiguedad. "))
+    años=float(input("digite años de antiguedad. "))
     if años==1:
         print(f"Trabajador {trabajador} recibe 6 dias de vacaciones. ")
     elif años>=2 and años<=6:
@@ -15,8 +19,7 @@ if clave_departamento==1:
         print("no tiene derecho a vacaciones")
 #LOGISTICA
 elif clave_departamento==2:
-    trabajador=input("nombre del trabajdor. ")  
-    años=int(input("digite años de antiguedad. "))
+   
     if años==1:
         print(f"Trabajador {trabajador} recibe 7 dias de vacaciones. ")
     elif años>=2 and años<=6:
@@ -26,9 +29,9 @@ elif clave_departamento==2:
     else:
         print("no tiene derecho a vacaciones")
 #GERENCIA
-elif clave_departamento==3:
-    trabajador=input("nombre del trabajdor. ")
-    años=int(input("digite años de antiguedad. "))
+else: 
+    clave_departamento==3
+    
     if años==1:
      print(f"Trabajador {trabajador} recibe 10 dias de vacaciones. ")
     elif años>=2 and años<=6:
@@ -37,6 +40,5 @@ elif clave_departamento==3:
         print(f"Trabajador {trabajador} recibe 30 dias de vacaciones. ")
     else:
         print("no tiene derecho a vacaciones")
-else:
-    print("¡ESTE DEPARTAMENTO NO ESTA DISPONIBLE!")
+
   
