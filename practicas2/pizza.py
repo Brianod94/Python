59307530436dbfa50df3a747ed0daa55a73a72ae
Bianod94 <brianoro312@gmail.com>
2025-03-25 11:08:42 -0500

    hoy

--- practicas2/pizza.py
+++ practicas2/pizza.py
@@ -2,35 +2,35 @@ while True:
     print("*****************************************************")
     print("**********Bienvenido a Pizza Bella Napoli************")
     print("¿Ingrese numero del Tipo pizza que desea ordenar?")# \n1. vegetariana\n2. No vegetariana\n3. Salir")
-    print("1. Vegetariana.")
-    print("2. No vegetariana.")
-    print("3. Salir")
+    print("[1] Vegetariana.")
+    print("[2] No vegetariana.")
+    print("[3] Salir")
     print("*****************************************************")
     tipo= input("¿opcion? = ")
     
     print("gracias por su visita")
     #desicion sobre el tipo de pizza 
     if tipo =="1":
-        print("ingrese el numero del ingrediente adicional\n1. pimiento\n2. tufo")
-        ingrediente=input("numero = ")
+        print("ingrese el numero del ingrediente adicional\n[1] pimiento\n[2] tufo")
+        ingrediente=input("¿opcion? = ")
         if  ingrediente=="1":
-            print("ustep ordeno una pizza vegetariena con mozarella,tomate y pimiento")
+            print("\nustep ordeno una pizza vegetariena con mozarella,tomate y pimiento\n")
         elif ingrediente=="2":
-            print("ustep ordeno una pizza vegetariena con mozarella,tomate y tofu")
+            print("\nustep ordeno una pizza vegetariena con mozarella,tomate y tofu\n")
         else:
-            print("este ingrediente no esta disponible")
+            print("\n!ERROR¡ ingrediente no encontrado\n")
 
     elif tipo =="2":
-        print("ingrese numero del ingrediente adiccional.\n1. Peperoni\n2. Jamon\n3. Salmon")
+        print("ingrese numero del ingrediente adiccional.\n[1] Peperoni\n[2] Jamon\n[3] Salmon")
         ingrediente = input("¿opcion? = ")
         if ingrediente=="1":
-            print("ustep ordeno una pizza no vegetariena y es con mozarella,tomate y peperoni")
+            print("\nustep ordeno una pizza no vegetariena y es con mozarella,tomate y peperoni\n")
         elif ingrediente=="2":
-            print("ustep ordeno una pizza no vegetariena y es con mozarella,tomate y jamon")
+            print("\nustep ordeno una pizza no vegetariena y es con mozarella,tomate y jamon\n")
         elif ingrediente=="3":
-            print("ustep ordeno una pizza no vegetariena y es con mozarella,tomate y salmon")
+            print("\nustep ordeno una pizza no vegetariena y es con mozarella,tomate y salmon\n")
         else:
-            print("ese ingrediente no esta disponible")
+            print("\n!ERROR¡ ingrediente no encontrado\n")
         
     else:
         
