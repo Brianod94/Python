631c65b13dbcd7ffd1fa1510c79d6a295a740750
Bianod94 <brianoro312@gmail.com>
2025-03-27 10:36:10 -0500

    guardar

--- practicas_while/compras.py
+++ practicas_while/compras.py
@@ -1,15 +1,14 @@
-salir = False
+salir = 1
 total_catidad=0
-while salir== False:
+while salir== 1:
 
     valor = int(input("digite el valor del articulo:\n"))
-    
     cantidad = int(input("cantidad de articulos:\n"))
     
     total_catidad = total_catidad+(valor*cantidad)
     print(f"acumulado. {total_catidad}\n")
 
-    pregunta=int(input("¿desea seguir comprando?.\n1. si\n2. no\n!opcion¡: ")) 
-    if pregunta==2: 
-        salir=True
-print(f"total compra es: {total_catidad}")   
+    salir=int(input("¿desea seguir comprando?.\n1. si\n2. no\n!opcion¡: ")) 
+    if salir==2: 
+        print(f"total compra es: {total_catidad}")   
+    
