9e7e5324e729a8d6a8417cd58031148ee9aba9fb
Bianod94 <brianoro312@gmail.com>
2025-03-30 16:26:19 -0500

    gu

--- practicas_while/ejemplos_while.py
+++ practicas_while/ejemplos_while.py
@@ -1,10 +1,3 @@
-# El ejercicio que resolvemos es:
-# Escribe un programa que, al recibir como dato un número entero positivo N, 
-# calcule el resultado de la siguiente serie:
-# n 1 + (1/2) + (1/3) + (1/4) + ... + (1/N)
-# Si el usuario escribe un número incorrecto, el programa no se ejecuta. 
-# En cambio, pregunta de nuevo por la información hasta que el dato ingresado sea correcto
-
 cambio =  True
 while cambio == True:
 
