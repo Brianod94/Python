03cf8c6624b4d5d0ab6cc54649c1e0b4cd41852b
Bianod94 <brianoro312@gmail.com>
2025-04-01 09:57:56 -0500

    cam

--- tablas_multiplicar.py
+++ tablas_multiplicar.py
@@ -1,12 +1,23 @@
-j=True
-while j==True:
-    uno=int(input("que tabla desea saber es: "))
-    if uno > 0:
+#j=True
+#while j==True:
+    #uno=int(input("que tabla desea saber es: "))
+    #if uno > 0:
+#
+        #i=1
+        #while i < 11:
+            #print(f"{uno} x {i} = {uno*i}")
+            #i+=1
+#        
+    #else:
+        #print("el numero debe ser mayor que 0.")
 
-        i=1
-        while i < 11:
-            print(f"{uno} x {i} = {uno*i}")
-            i+=1
-        
-    else:
-        print("el numero debe ser mayor que 0.")
+# otra solucion para este codigo es.
+numero=int(input("que tabla desea saber es: "))
+
+limite=10
+i=1
+while i <=limite:
+    resultado= i*numero
+    print(f"{i} x {numero}= {resultado}")
+    i+=1
+
