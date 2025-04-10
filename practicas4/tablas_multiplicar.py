dfc68aad5bf7e67305ac2aba5f0944843c7405ee
Brianod94 <brianoro312@gmail.com>
2025-04-09 20:09:31 -0500

    Update tablas_multiplicar.py

--- practicas4/tablas_multiplicar.py
+++ practicas4/tablas_multiplicar.py
@@ -21,3 +21,12 @@
     #print(f"{i} x {numero}= {resultado}")
     #i+=1
 
+j=1
+while j<=10:
+    i=1 
+    while i <= 10:
+        print(f"{j} * {i} = {j*i}")
+        
+        i+=1 
+    j+=1
+    print("===============================")
