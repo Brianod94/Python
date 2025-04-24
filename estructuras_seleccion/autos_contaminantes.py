e47374f6e078fb2497f274d5e6b4cc96c7024dba
Bianod94 <brianoro312@gmail.com>
2025-04-24 10:10:54 -0500

    new

--- estructuras_seleccion/autos_contaminantes.py
+++ estructuras_seleccion/autos_contaminantes.py
@@ -6,16 +6,18 @@ minimo = 1000
 
 for i in range (1,6):
     cantidad_p_cont = int (input(f"digite cantidad de puntos contaminantes del vehiculo {i}. ")) #cantidad de puntos contaminantes
+    
     promedio = cantidad_p_cont/5
-    if cantidad_p_cont > maximo:
+
+    if cantidad_p_cont > maximo: 
         maximo = cantidad_p_cont
     if cantidad_p_cont < minimo:
         minimo = cantidad_p_cont
    
 
-print(f"el valor maximo es: {maximo}")
-print(f"el valor minimo es: {minimo}")
-print(f"el promedio es: {promedio}")
+print(f"numero de puntos que mayor contamino: {maximo}")
+print(f"numero de puntos que menor contamino: {minimo}")
+print(f"el promedio de puntos contaminantes: {promedio}")
     
 
    
