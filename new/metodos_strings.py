d1829263d89af513c8d93b9653130fb879c864f2
Bianod94 <brianoro312@gmail.com>
2025-06-22 14:26:51 -0500

    h

---
+++ new/metodos_strings.py
@@ -0,0 +1,6 @@
+my_nombre = "    brian orozco     "
+print(my_nombre.upper())    #convierte el texto en mayus
+print(my_nombre.lower())    #convierte el texto en minus
+print(my_nombre.strip().capitalize())   #convierte solo la primera letra en mayus
+print(my_nombre.title())   #convierte la primera letra de cada palabra en mayus
+print(my_nombre.strip())    # elimina los espacos de cada costado del texto
